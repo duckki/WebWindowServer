7ea17a8b8d88e76ca66ab2af35895ca6bf5e75aa
Duckki Oe <kain.nomad@gmail.com>
2020-09-13 12:25:06 -0700

    Initial commit with a miminum functionality.

---
+++ main.js
@@ -0,0 +1,251 @@
+// Web Window Server
+// Author: Duckki Oe
+
+//////////////////////////////////////////////////////////////////////////////
+// Module imports
+
+const { app, protocol, session, BrowserWindow } = require('electron')
+const http = require('http');
+const url = require('url');
+const { HTTPParser } = require('http-parser-js');
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Debugging
+
+console.log( "app: initializing." );
+
+verbose = false
+
+function debugLog()
+{
+    console.log( "debug:", ...arguments )
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+// WSession - Per-session data
+
+class WSession {
+    constructor( partition, sock ) {
+        this.partition = partition
+        this.socket = sock
+        this.responseCallbackQueue = []
+    }
+
+    toString() {
+        return "[" + this.socket.remoteAddress + ":" + this.socket.remotePort
+             + " " + this.partition
+             + "]"
+    }
+
+    queueCallback( callback ) {
+        this.responseCallbackQueue.push( callback )
+    }
+
+    onMessage( arg ) {
+        const callback = this.responseCallbackQueue[0]
+        callback( arg )
+        this.responseCallbackQueue.shift()
+    }
+}
+
+// translate the `ptth://` request into a HTTP request
+function handlePtthProtocol( wws_session, req, callback ) {
+    debugLog( wws_session + " handlePtthProtocol: " + req.url )
+    const url_obj = new url.URL( req.url )
+    const req_path = url_obj.pathname
+
+    const socket = wws_session.socket
+    socket.write('GET ' + req_path + ' HTTP/1.1\r\n\r\n')
+
+    // TODO: check the return value from `write`
+
+    wws_session.queueCallback( callback )
+}
+
+function initSocketDataHandler( wws_session )
+{
+    // per-session data
+    const parser = new HTTPParser(HTTPParser.RESPONSE)
+    var contentType
+    var contentBody
+
+    parser.onHeadersComplete = function(res) {
+        contentType = "text/plain" // default
+        contentBody = Buffer.alloc(0)
+
+        debugLog( wws_session + " receiving response: " + res.statusCode + " " + res.statusMessage )
+        if( verbose ) {
+            for( var k in res ) {
+                debugLog( "  " + k + ": " + res[k] )
+            }
+        }
+        //debugLog("headers:")
+        for( var i=0; i<res.headers.length; i+=2 ) {
+            const header_key = res.headers[i]
+            const header_val = res.headers[i+1]
+            //debugLog( "  " + header_key + ": " + header_val )
+            if( header_key == "Content-Type" ) {
+                contentType = header_val
+            }
+        }
+    }
+
+    parser.onBody = function(chunk, offset, length) {
+        const data = Buffer.from( chunk.buffer, offset, length )
+        contentBody = Buffer.concat( [contentBody, data] )
+    }
+
+    parser.onMessageComplete = function() {
+        debugLog( wws_session + " completed response message (" + contentType + ", size: " + contentBody.length + ")" )
+        wws_session.onMessage( { mimeType: contentType, data: contentBody } )
+    }
+
+    wws_session.socket.on( 'data', function(data) {
+        parser.execute( data )
+    })
+}
+
+function createSessionWindow( wws_session ) {
+    console.log( wws_session + " creating session window." );
+
+    // Create the browser window.
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            worldSafeExecuteJavaScript: true,
+            partition: wws_session.partition
+        }
+    })
+
+    if( verbose ) {
+        win.webContents.openDevTools();
+    }
+
+    const sock = wws_session.socket
+    sock.on( 'end', () => {
+        console.log( wws_session + " connection ended." );
+        if( ! win.isDestroyed() ) {
+            win.destroy()
+        }
+    })
+    sock.on( 'close', () => {
+        console.log( wws_session + " connection closed." );
+        if( ! win.isDestroyed() ) {
+            win.destroy()
+        }
+    })
+
+    win.on( 'closed', () => {
+        wws_session.socket.destroy()
+    })
+
+    console.log( wws_session + " loading initial page" )
+    win.loadURL('ptth://localhost/') // default url
+}
+
+function initSession( socket )
+{
+    // create a new partition & session object
+    const partition = Math.random().toString(36).substring(2, 15)
+    const wws_session = new WSession( partition, socket )
+
+    // "ptth" protocol handler
+    const ses = session.fromPartition( partition )
+    ses.protocol.registerBufferProtocol('ptth', (req, callback) => {
+        handlePtthProtocol( wws_session, req, callback );
+    })
+
+    // setup socket data handlers
+    initSocketDataHandler( wws_session )
+
+    // create a new session window
+    createSessionWindow( wws_session );
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Reverse-HTTP server
+
+function socketName(socket)
+{
+    return "[" + socket.remoteAddress + ':' + socket.remotePort + "]"
+}
+
+const server = http.createServer(function (req, res) {
+    debugLog( socketName(req.socket), 'unexpected HTTP request: ' + req.method + " " + req.url )
+    req.destroy()
+})
+
+server.keepAliveTimeout = 0
+server.timeout = 0
+
+server.on( 'connection', ( socket ) => {
+    debugLog( socketName(socket), 'incoming connection' )
+})
+
+server.on( 'upgrade', function( req, socket, head ) {
+    debugLog( socketName(socket), "upgrade requested: " + req.method + " " + req.url )
+
+    // TODO: verify request
+
+    // acknowledge upgrade
+    socket.write('HTTP/1.1 101 Switching Protocols\r\n' +
+        'Upgrade: PTTH/1.0\r\n' +
+        'Connection: Upgrade\r\n' +
+        '\r\n')
+
+    initSession( socket );
+})
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Main window
+// - Currently, this is just a splash window.
+// - In future, this may be used for window management (hidden by default).
+
+function createMainWindow() {
+    console.log( "app: creating main window." );
+
+    // Create the browser window.
+    const win = new BrowserWindow({
+        width: 400,
+        height: 300,
+        webPreferences: {
+            worldSafeExecuteJavaScript: true,
+            nodeIntegration: false
+        }
+    })
+
+    if( verbose ) {
+        win.webContents.openDevTools();
+    }
+
+    win.loadFile('main-window.html')
+}
+
+function startServer()
+{
+    console.log( "app: starting http server." );
+    server.listen( 8080, "localhost" )
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Initialization
+
+protocol.registerSchemesAsPrivileged([
+    {
+        scheme: 'ptth',
+        privileges: {
+            standard: true // allow relative URLs
+        }
+    }
+])
+
+app.whenReady().then(createMainWindow)
+               .then(startServer)
+
+console.log( "app: finished initialization." );
---
+++ tests/test-client-app.js
@@ -0,0 +1,91 @@
+const http = require('http');
+const fs = require('fs');
+const { createCanvas } = require('canvas')
+const reverseHttp = require('reverse-http')
+
+console.log( "app:", "initializing." )
+
+function debugLog()
+{
+    console.log( "debug:", ...arguments )
+}
+
+// Test HTML page
+const index_page = "<html><body>"
+                 + "<p>Test client index page"
+                 + "<p>Date: " + new Date().toDateString()
+                 + "<p>Time: " + new Date().toTimeString()
+                 + "<p><img src=\"test.jpg\">"
+                 + "</body></html>"
+
+// Returns a Buffer with a PNG image.
+function createTestPngImage()
+{
+    const width = 600
+    const height = 250
+
+    const canvas = createCanvas(width, height)
+    const context = canvas.getContext('2d')
+
+    context.fillStyle = '#999'
+    context.fillRect(0, 0, width, height)
+
+    const text = 'Generated\nTest\nImage\n'
+               + new Date().toDateString() + "\n"
+               + new Date().toTimeString()
+    context.font = 'bold 20pt Verdana'
+    context.textAlign = 'left'
+    context.textBaseline = 'top'
+    context.fillStyle = '#000'
+    context.fillText(text, 20, 20)
+
+    return canvas.toBuffer('image/png')
+}
+
+// Connection options
+const opts = {
+    hostname: 'localhost',
+    port: 8080,
+    method: 'GET',
+    path: '/'
+}
+
+// Open an HTTP connection to example.com and accept reverse HTTP
+// requests back to this machine
+const conn = reverseHttp(opts, function (req, res) {
+    console.log( 'incoming request:', req.method, req.url)
+
+    if( req.url == "/" ) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+
+        res.end(index_page)
+    }
+    else if( req.url == "/test.jpg" ) {
+        const img_buf = createTestPngImage()
+        res.setHeader('Content-Type', 'image/png');
+        res.end(img_buf);
+    }
+    else {
+        res.setHeader('Content-Type', 'text/plain');
+        res.statusCode = 404;
+        res.end('Not found');
+    }
+})
+
+// This is necessary to keep the connection alive (maybe one of them).
+conn.keepAliveTimeout = 0
+conn.timeout = 0
+
+conn._socket.on('end', () => {
+    console.log( "app:", "connection ended" )
+    process.exit(0)
+})
+
+conn._socket.on('close', () => {
+    console.log( "app:", "connection closed" )
+    process.exit(0)
+})
+
+console.log( "app:", "started." )
