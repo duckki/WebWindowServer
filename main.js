10dcd1106e28625ea1eebcfd4a3d0b33e2da4a9e
Duckki Oe <kain.nomad@gmail.com>
2020-09-13 13:36:12 -0700

    added license statement

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // Web Window Server
-// Author: Duckki Oe
 
 //////////////////////////////////////////////////////////////////////////////
 // Module imports
+
+console.log( "app: initializing." );
 
 const { app, protocol, session, BrowserWindow } = require('electron')
 const http = require('http');
@@ -12,8 +13,6 @@ const { HTTPParser } = require('http-parser-js');
 
 //////////////////////////////////////////////////////////////////////////////
 // Debugging
-
-console.log( "app: initializing." );
 
 verbose = false
 
@@ -249,3 +248,19 @@ app.whenReady().then(createMainWindow)
                .then(startServer)
 
 console.log( "app: finished initialization." );
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Copyright 2020 Duckki Oe
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
